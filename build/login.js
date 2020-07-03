d5c06bf12be925e03cbaa28e04310ee0921a15c1
Mahdi Qazwini <42902912+qazwini@users.noreply.github.com>
2020-07-03 17:26:15 -0400

    Added login page and created new login.js

---
+++ build/login.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAMXxjjvlDrdky2yEhqiEH3nGwZEC8Y2_k",
+  authDomain: "grand-giveaways.firebaseapp.com",
+  databaseURL: "https://grand-giveaways.firebaseio.com",
+  projectId: "grand-giveaways",
+  storageBucket: "grand-giveaways.appspot.com",
+  messagingSenderId: "379194298530",
+  appId: "1:379194298530:web:fffb22c1dfa431867a07fb",
+  measurementId: "G-YXLCT22G1W"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
