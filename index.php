b57fd372fdcd4d4f2cc8c40601dc236e70a7ca97
qazwini <mmqazwini@yahoo.com>
2021-03-09 01:15:20 -0500

    changes

--- index.php
+++ index.php
@@ -1,79 +1,39 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
-    <link rel="stylesheet" type="text/css" href="/css/header&footer.css">
-    <title>Grand Giveaways</title>
-  </head>
-  <body style="background-color: #E90A43">
+<!-- <?php session_start();?> -->
 
-    <?php include('templates/headerV2.php') ?>
+<?php
+  $_GET['trans']=true;
+  include('templates/header.php');
+?>
 
-    <?php
-    if (isset($_SESSION["name"])) {
-        echo "<p>Hello there " . $_SESSION["name"] . "</p>";
-    } else {
-      echo "<p>:(</p>";
-    }
-     ?>
+<section id="front" class="hp_front">
+  <div class="center">
+    <h1 class="hp_front_title">
+        Welcome to Grand Giveaways.
+    </h1>
+    <h2 class="hp_front_subtitle">
+      A platform to win big prizes
+      <br>
+      from your favorite influencers.
+    </h2>
+  </div>
+</section>
 
-    <div class="center">
-      <div class="mainarea" style="margin-bottom:30px;">
+<section id="about" class="hp_body">
+  <div class="center">
 
-        <a href="page.php">
-          <div class="card cleft">
-              <img src="stuff/Apple.jpg" class="cardimg">
-              <div class="cardpersondiv">
-                <img src="stuff/MKBHD Profile Pic.jpg" class="cardpersonimg">
-              </div>
-              <div class="carddetails">
-                <p class="cardtitle"> The iPhone 11 256GB + AirPods & Charging Pad Giveaway
-                </p>
-                <p class="cardpersonname"> Marques Brownlee</p>
-              </div>
-            </div>
-        </a>
+  </div>
+</section>
 
-         <div class="card">
-            <img src="stuff/img.jpg" class="cardimg">
-            <div class="cardpersondiv">
-              <img src="stuff/person.jpg" class="cardpersonimg">
-            </div>
-            <div class="carddetails">
-              <p class="cardtitle">HBO Now, Apple TV+, Hulu, & Disney+</p>
-              <p class="cardpersonname">Mike Pence</p>
-            </div>
-          </div>
+<section id="influencers" class="hp_body">
+  <div class="center">
 
-        <div class="card cleft">
-          <img src="stuff/Ryan's toys.jpg" class="cardimg">
-          <div class="cardpersondiv">
-            <img src="stuff/ryan.jpg" class="cardpersonimg">
-          </div>
-          <div class="carddetails">
-            <p class="cardtitle">Ryan's Mystery Toy Box</p>
-            <p class="cardpersonname">Ryan Kaji</p>
-          </div>
-        </div>
+  </div>
+</section>
 
-        <div class="card">
-          <img src="stuff/Knives by GR.jpg" class="cardimg">
-          <div class="cardpersondiv">
-            <img src="stuff/Ramsay.jpg" class="cardpersonimg">
-          </div>
-          <div class="carddetails">
-            <p class="cardtitle">Gordon Ramsay by Royal Doulton® 6-Piece Knife Block Set</p>
-            <p class="cardpersonname">Gordon Ramsay</p>
-          </div>
-        </div>
+<section id="users" class="hp_body">
+  <div class="center">
 
-      </div>
-    </div>
+  </div>
+</section>
 
-
-    <?php include('templates/footer.php') ?>
-
-  </body>
-</html>
+<?php include('templates/footer.php') ?>
--- templates/header.php
+++ templates/header.php
@@ -1,72 +1,33 @@
-<!-- <nav class="navbar navbar-expand-lg">
-  <a class="navbar-brand navfont navlogo" href="/">Grand Giveaway</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Grand Giveaways</title>
+    <link rel="stylesheet" href="/style.css">
+  </head>
+  <body>
 
-      <ul class="navbar-nav mr-auto">
-      </ul>
 
-      <ul class="navbar-nav ">
-          <li class="nav-item active">
-               <a class="navfont nav-link" id= "give" href="/">GIVEAWAYS <span class="sr-only">(current)</span></a>
-          </li>
-            <li class="nav-item">
-              <a class="navfont nav-link" id ="freebie" href="#">FREEBIES</a>
-            </li>
-      </ul>
 
-    <ul class="navbar-nav navfont" id="profile">
-         <li class="nav-item dropdown">
-        <a href="login.php"><img src="stuff/profile.png" class="nav-link dropdown-toggle pic2" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Profile</a>
-          <a class="dropdown-item" href="#">Account</a>
-          <a class="dropdown-item" href="#">Purchases</a>
-
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Sign Out</a>
-        </div>
-      </li>
-  </ul>
-</div>
-</nav> -->
-
-<div class="ggnavbar">
-  <div class="center">
-  <a class="navbar-brand navfont navlogo" href="/">Grand Giveaway</a>
-  <a href="login.php" style="color:white; float:right;">Log in</a>
-  <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-      <ul class="navbar-nav mr-auto">
-      </ul>
-
-      <ul class="navbar-nav ">
-          <li class="nav-item active">
-               <a class="navfont nav-link" id= "give" href="/">GIVEAWAYS <span class="sr-only">(current)</span></a>
-          </li>
-            <li class="nav-item">
-              <a class="navfont nav-link" id ="freebie" href="#">FREEBIES</a>
-            </li>
-      </ul>
-
-    <ul class="navbar-nav navfont" id="profile">
-         <li class="nav-item dropdown">
-        <a href="login.php"><img src="stuff/profile.png" class="nav-link dropdown-toggle pic2" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Profile</a>
-          <a class="dropdown-item" href="#">Account</a>
-          <a class="dropdown-item" href="#">Purchases</a>
-
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Sign Out</a>
-        </div>
-      </li>
-  </ul>
-</div> -->
-</div>
-</div>
+    <div class="header <?php if (isset($_GET['trans'])) { if ($_GET['trans'] === true) { echo "trans"; } } ?>">
+      <div class="header_left">
+        <img class="header_logo" src="/resources/logo_white.svg">
+      </div>
+      <div class="header_right">
+        <?php
+        if (isset($_SESSION["username"])) {
+          echo "<a href='/account.php' class='header_user'>Hi " . $_SESSION["username"] . "!</a>";
+        } else {
+          echo <<< EOD
+          <ul class="header_menu">
+            <li class="header_menu_item"><a href="/" class="header_menu_item_a">Home</a></li>
+            <li class="header_menu_item"><a href="/influencers.php" class="header_menu_item_a">Influencers</a></li>
+            <li class="header_menu_item"><a href="/users.php" class="header_menu_item_a">Users</a></li>
+            <li class="header_menu_item"><a href="/login.php" class="header_menu_item_a">Log in</a></li>
+          </ul>
+          <a href="/register.php" class="header_register">Register</a>
+          EOD;
+        }
+        ?>
+      </div>
+    </div>
