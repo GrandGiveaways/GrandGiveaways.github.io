714a5870401045f8a8bf5664f94244d4d5e31e71
Mahdi Qazwini <mqazwini@umich.edu>
2021-03-28 14:46:28 -0400

    Fix hompeage again

--- index.php
+++ index.php
@@ -40,50 +40,54 @@ while ($row = $result->fetch_assoc()) {
 </div>
 -->
 
-<section id="about" class="hp_body">
-  <div class="center">
+<div id="hp_main">
+
+  <section id="about" class="hp_body">
+    <div class="center">
       <div class="homeinfo">
         <img class="giveawayImg" src="/resources/giveawayImg.jpg">
         <div class="heading">
-            <h3><b>Who Are We?</b></h3>
+          <h3><b>Who Are We?</b></h3>
         </div>
         <br>
         <div class="description">
-            <p>We had an idea that sought to bring users and influencers together for the common good. We created a platform that allowed both parties to benefit from one another. We value the exciting opportunities that giveaways present for both parties.</p>
+          <p>We had an idea that sought to bring users and influencers together for the common good. We created a platform that allowed both parties to benefit from one another. We value the exciting opportunities that giveaways present for both parties.</p>
         </div>
       </div>
-  </div>
-</section>
+    </div>
+  </section>
 
-<section id="influencers" class="hp_body">
-  <div class="center">
+  <section id="influencers" class="hp_body">
+    <div class="center">
       <div class="homeinfo">
         <img class="influencerGif" src="/resources/Influencers.gif">
         <div class="heading">
-            <h3><b>What We Do For Influencers</b></h3>
+          <h3><b>What We Do For Influencers</b></h3>
         </div>
         <br>
         <div class="description">
-            <p>We know you love your followers, here at Grand Giveaway we provide a quick, simple, and reliable way for you to reward them while promoting your brand.</p>
+          <p>We know you love your followers, here at Grand Giveaway we provide a quick, simple, and reliable way for you to reward them while promoting your brand.</p>
         </div>
       </div>
-  </div>
-</section>
+    </div>
+  </section>
 
-<section id="users" class="hp_body">
-  <div class="center">
+  <section id="users" class="hp_body">
+    <div class="center">
       <div class="homeinfo">
         <img class="bouncingGift" src="/resources/bouncingGift.gif">
         <div class="heading">
-            <h3><b>Support Your Favorite Creators and Win!</b></h3>
+          <h3><b>Support Your Favorite Creators and Win!</b></h3>
         </div>
         <br>
         <div class="description">
-            <p>Who doesn’t like free stuff? With Grand Giveaway, we make it easier than ever for you to sign up for, and win, giveaways from your favorite influencers. To win prizes, you simply need to press “enter” and watch a short ad. All you need is your email and a few seconds to get started.</p>
+          <p>Who doesn’t like free stuff? With Grand Giveaway, we make it easier than ever for you to sign up for, and win, giveaways from your favorite influencers. To win prizes, you simply need to press “enter” and watch a short ad. All you need is your email and a few seconds to get started.</p>
         </div>
       </div>
-  </div>
-</section>
+    </div>
+  </section>
+
+</div>
 
 <?php
   require_once 'templates/footer.php';
