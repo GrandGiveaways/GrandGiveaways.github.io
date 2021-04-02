21021107bf33e223e6883805d993719f7ef4e21f
Mahdi Qazwini <mqazwini@umich.edu>
2021-04-02 14:10:54 -0400

    Add shortcut icon

--- templates/header.php
+++ templates/header.php
@@ -6,7 +6,9 @@
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="/style.css">
-    <!-- Google Analytics -->
+    <link rel="shortcut icon" type="image/svg" href="/resources/logo_white.svg">
+    <!-- Google - Start -->
+    <script data-ad-client="ca-pub-1314468233933209" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z35JC3QW68"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
@@ -14,11 +16,8 @@
       gtag('js', new Date());
       gtag('config', 'G-Z35JC3QW68');
     </script>
-    <!-- Google Analytics -->
+    <!-- Google - End -->
     <?php echo $additional; ?>
-
-    <!-- Google AdSense -->
-    <script data-ad-client="ca-pub-1314468233933209" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
   </head>
 
   <body>
