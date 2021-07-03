5a570a828ff0296f2c7dc62408e4c6f87174bf70
Hussein Harajli <59298179+GrandGiveaways@users.noreply.github.com>
2021-07-03 20:17:50 +0300

    Remove Old Header/Footer CSS

--- My Account.php
+++ My Account.php
@@ -4,7 +4,6 @@
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/My Account.css">
-	<link rel="stylesheet" type="text/css" href="css/header&footer.css">
 	<title>My Account</title>
 
 </head>
