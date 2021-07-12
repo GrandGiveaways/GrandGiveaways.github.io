35198e365f8ad5c1d40201fb3f2c0213771e07c9
Mahdi Qazwini <mqazwini@umich.edu>
2021-07-12 19:02:40 -0400

    Add bootstrap (should be removed) v2

--- account.php
+++ account.php
@@ -1,8 +1,8 @@
 <?php
 	$title = "My Account";
+	$additional = `<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">`;
 	require_once 'templates/page_header.php';
 	$caption = $_SESSION["username"];
-	$additional = `<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">`;
 ?>
 
 <div class="topNav d-block d-md-none">
